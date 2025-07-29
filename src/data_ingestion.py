4f308e376a2c2e0815120238cf6245ef5a4378ae
saim-x <x_aci@hotmail.com>
2025-07-29 16:12:09 +0500

    completed data ingestion module

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -21,10 +21,7 @@ console_handler.setFormatter(formatter)
 file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
-logger.setFormatter(formatter)
-
 logger.addHandler(file_handler)
-logger.addHandler(console_handler)
 
 def load_data(data_url: str)-> pd.DataFrame:
     try:
@@ -38,3 +35,44 @@ def load_data(data_url: str)-> pd.DataFrame:
         logger.error(f"Error loading data from {data_url}: {e}")
         raise e
 
+def preprocess_data(df: pd.DataFrame)-> pd.DataFrame:
+    try:
+        df.drop(columns=['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], inplace=True)
+        df.rename(columns= {'v1': 'target', 'v2': 'text'}, inplace=True)
+        logger.debug("Data preprocessed successfully")
+        return df
+    except KeyError as e:
+        logger.error('Missing column in the dataframe: %s', e)
+        raise
+    except Exception as e:
+        logger.error(f"Error preprocessing data: {e}")
+        raise
+
+def save_data(train_data: pd.DataFrame, test_data:pd.DataFrame, data_path: str) -> None:
+    try:
+        raw_data_path = os.path.join(data_path, 'raw')
+        os.makedirs(raw_data_path, exist_ok=True)
+        train_data.to_csv(os.path.join(raw_data_path, "train.csv"), index=False)
+        test_data.to_csv(os.path.join(raw_data_path, "test.csv"), index=False)
+        logger.debug(f"Train and test data saved to %s", raw_data_path)
+    except Exception as e:
+        logger.error(f"Error saving data: {e}")
+        raise
+    
+    
+def main():
+    try:
+        test_size = 0.2
+        data_path = 'https://raw.githubusercontent.com/saim-x/ML-Pipeline-AWS-and-DVC/refs/heads/main/experiments/spam.csv'
+        df = load_data(data_url=data_path)
+        final_df = preprocess_data(df)
+        train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=2)
+        save_data(train_data, test_data, data_path='./data')
+    except Exception as e:
+        logger.error("Failed to complete the data ingestion process %s", e)
+        raise
+    
+if __name__ == "__main__":
+    main()
+    
+        
